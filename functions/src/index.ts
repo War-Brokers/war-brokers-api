ef15af505326f5d404d2312eb059adfe70669be7
developomp <developomp@gmail.com>
2023-06-21 18:36:14 +0900

    fix: attempt to fix github action

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,7 @@
+import { setGlobalOptions } from "firebase-functions/v2"
+
+// Set the maximum instances to 10 for all functions
+// limitations set by quota
+setGlobalOptions({ maxInstances: 10 })
+
 export * from "./ping"
